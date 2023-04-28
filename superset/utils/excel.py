6f2cc1272c580af5703ec7743ff3b0ab044cc9d2
akinyabaev <ayrat.kinyabaev@mail.ru>
2023-04-28 22:44:02 +0300

    correcting excel loading

--- superset/utils/excel.py
+++ superset/utils/excel.py
@@ -77,7 +77,7 @@ def df_to_escaped_csv(df: pd.DataFrame, **kwargs: Any) -> Any:
                 if isinstance(value, str):
                     df.at[idx, name] = escape_value(value)
 
-    return df.to_csv(**kwargs)
+    return df.to_excel(**kwargs)
 
 
 def get_chart_csv_data(
